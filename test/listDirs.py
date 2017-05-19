e93f9c6ea76efa9dbcffc29e3385b7ac9baaa937
Chen Jiadong <coolboy096188@gmail.com>
2017-05-19 20:56:59 +0900

    [add]change file name like Rags (Rags/Riches) to RagsRiches

--- test/listDirs.py
+++ test/listDirs.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 files = os.listdir("/Users/coolboy096188/Library/Caches/Forge/pics/cards")
 str = ""
@@ -9,3 +10,15 @@ print(str)
 str2 = "ab"
 str2 = str2.replace("/", ":")
 print(str2)
+
+
+#pattern = "[A-Za-Z]+ \(([A-Za-Z]+)/([A-Za-Z]+)\)"
+pattern = "^[a-zA-Z]+ \(([a-zA-Z]+.*)/([a-zA-Z]+.*)\)"
+text = "Rags (Rags abc/Riches bbb)"
+repatter = re.compile(pattern)
+matchOB = repatter.match(text)
+if matchOB:
+    print matchOB.group(0)
+    print matchOB.group(1)
+    print matchOB.group(2)
+    print matchOB.group(1) + matchOB.group(2)
